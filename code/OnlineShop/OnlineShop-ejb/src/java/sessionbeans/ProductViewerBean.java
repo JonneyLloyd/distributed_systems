86eb401545491b4b512ca3fbd720231a85a2df70
JonneyLloyd <jonneylloyd@yahoo.co.uk>
2017-04-14 15:49:35 +0000

    Jonney missing code

--- code/OnlineShop/OnlineShop-ejb/src/java/sessionbeans/ProductViewerBean.java
+++ code/OnlineShop/OnlineShop-ejb/src/java/sessionbeans/ProductViewerBean.java
@@ -27,6 +27,12 @@ public class ProductViewerBean {
     @PersistenceContext(unitName = "OnlineShop-ejbPU")
     private EntityManager em;
 
+    /**
+     * Get a product by its name
+     * 
+     * @param name string containing name of the product
+     * @return List of Product objects with matching name
+     */
     public List<Product> getProductByName(String name) {
         // create named query and set parameter
         Query query = em.createNamedQuery(
@@ -34,8 +40,30 @@ public class ProductViewerBean {
                 .setParameter("name", name);
         // return query result
         return query.getResultList();
+
     }
     
+    /**
+     * Search for products with name
+     * Uses SQL LIKE function for close matches
+     * 
+     * @param name string containing name of the product
+     * @return List of Product objects with similar name
+     */
+    public List<Product> searchProductByName(String name) {
+        // create named query and set parameter
+        Query query = em.createNamedQuery(
+                "Product.searchByName")
+                .setParameter("name", name);
+        // return query result
+        return query.getResultList();
+    }
+    
+    /**
+     * Gets all products in the database
+     * 
+     * @return List object of Product objects
+     */
     public List<Product> getAllProducts() {
         // create named query and set parameter
         Query query = em.createNamedQuery(
@@ -44,11 +72,17 @@ public class ProductViewerBean {
         return query.getResultList();
     }
     
-    public List<Product> getAllProductsByCatagory(String catagory) {
+    /**
+     * Search for products with category
+     * 
+     * @param catagory string containing name of the category
+     * @return List of Product objects with same category
+     */
+    public List<Product> getAllProductsByCategory(String category) {
         // create named query and set parameter
         Query query = em.createNamedQuery("Catagory.findByDescription");
             //setting the provided parameters on the query
-            query.setParameter("description", catagory);
+            query.setParameter("description", category);
             //return result of query
             List<Product> productMatch = null;
             List<Catagory> catagoryMatch =  query.getResultList();
