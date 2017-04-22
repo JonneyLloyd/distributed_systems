25d24c64e666215896817960d641bbcf6de134b7
OliverGavin <14161044@studentmail.ul.ie>
2017-04-22 22:27:10 +0100

    Docs and tidy for basket

--- code/OnlineShop/OnlineShop-war/src/java/managedbeans/ProductViewer.java
+++ code/OnlineShop/OnlineShop-war/src/java/managedbeans/ProductViewer.java
@@ -49,6 +49,11 @@ public class ProductViewer implements Serializable {
     public ProductViewer() {
     }
 
+    /**
+     * Initializes data members post construction, after EJB's have been injected.
+     * Populates a list of Products in a ProductWrapper with a use selected quantity.
+     * Populates a list of categories for filtering.
+     */
     @PostConstruct
     public void init() {
         productFacade.findAll(false).forEach((p) -> {
@@ -89,10 +94,18 @@ public class ProductViewer implements Serializable {
         this.name = name;
     }
     
+    /**
+     * Getter for id variable
+     * @return  The id for filtering
+     */
     public Integer getId() {
         return id;
     }
     
+    /**
+     * Setter for id variable
+     * @param id    The id for filtering
+     */
     public void setId(Integer id) {
         this.id = id;
     }
@@ -105,10 +118,17 @@ public class ProductViewer implements Serializable {
         return this.entryList;
     }
     
+    /**
+     * Get all categories in database
+     * @return List of Category objects
+     */
     public List<Catagory> getAllCategories() {
         return this.categoryList;
     }
     
+    /**
+     * Filter the product list based on id, category and name values.
+     */
     public void filterProducts(){
         boolean deleted = false;
         productFacade.findByFilter(this.id, this.category, this.name, deleted).forEach((p) -> {
@@ -116,7 +136,11 @@ public class ProductViewer implements Serializable {
         });
     }
     
-    public void saveProduct(ProductWrapper productWrapper){
+    /**
+     * Save modifications to a product in a row of the data table.
+     * @param productWrapper    The wrapper containing the product
+     */
+    public void saveProduct(ProductWrapper productWrapper) {
         productFacade.edit(productWrapper.getProduct());
     }
     
@@ -125,6 +149,10 @@ public class ProductViewer implements Serializable {
         entryList.remove(productWrapper);
     }
     
+    /**
+     * Add a product and it's selected quantity in a row of the data table to the user's basket.
+     * @param productWrapper    The wrapper containing the product and the selected quantity
+     */
     public void addToBasket(ProductWrapper productWrapper){
         userBasketManager.addProduct(productWrapper.getProduct(), productWrapper.getSelectedQty());
         productWrapper.setSelectedQty(0);
--- code/OnlineShop/OnlineShop-war/src/java/managedbeans/UserBasketManager.java
+++ code/OnlineShop/OnlineShop-war/src/java/managedbeans/UserBasketManager.java
@@ -1,27 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package managedbeans;
 
 import entities.Product;
 import entities.StoredBasket;
 import entities.User;
 import javax.inject.Named;
-import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.List;
-import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import sessionbeans.UserBasketBean;
 
 /**
- *
- * @author oligavin
+ * Managed bean for handling operations on a user's basket
  */
 @Named(value = "userBasketManager")
 @RequestScoped
@@ -39,17 +30,28 @@ public class UserBasketManager implements Serializable {
     public UserBasketManager() {
     }
     
+    /**
+     * Gets all the entries (products) in a user's basket
+     * @return list of entries
+     */
     public List<StoredBasket> getAllProducts() {
-//        return this.storedBasketList;
         return (List<StoredBasket>) loginBean.getLoggedInUser().getUserBasket().getStoredBasketCollection();
     }
     
+    /**
+     * Add a product with a given quantity to the current user's basket
+     * @param product   The product to add
+     * @param qty       The quantity of the product
+     */
     public void addProduct(Product product, int qty) {
-        System.out.println(">>>>>>>>>>>>Adding with qty: "+qty);
         User user = loginBean.getLoggedInUser();
         userBasketBean.addProduct(user, product, qty);
     }
     
+    /**
+     * Remove an entry in the users basket
+     * @param storedBasket  The entry (mapping user's basket and product)
+     */
     public void removeStoredBasket(StoredBasket storedBasket) {
         userBasketBean.removeStoredBasket(storedBasket);
     }
